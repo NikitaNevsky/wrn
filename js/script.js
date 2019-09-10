7991535e793466106460cf151a96d1867b7ec736
NikitaNevsky <40342871+NikitaNevsky@users.noreply.github.com>
2019-09-10 19:32:57 +0300

    Upload files

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-$(function () {
-    $('select').selectpicker();
-});
-
 $(document).ready(function () {    
 
     $('select').selectpicker();
     
+});
+
+$(window).resize(function () {
+    var width = $('body').innerWidth()
+    if (width < 426) {
+        $('.plans-list').slick({
+            dots: true,
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        });
+    }
+
 });
